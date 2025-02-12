1d7b419a244086e0dc36d34e9fabff8669c3ff1b
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-02-12 16:47:04 +0545

    cmt

--- script.js
+++ script.js
@@ -10,34 +10,6 @@ function updateStorage(){
     localStorage.setItem("notes",notesContainer.innerHTML);
 }
 
-// createBtn.addEventListener("click",()=>{
-//     let inputBox=document.createElement("p");
-//     let img=document.createElement("img");
-//     inputBox.className="input-box"
-//     inputBox.setAttribute("contenteditable","true")
-//     img.src="delete.png"
-//     notesContainer.appendChild(inputBox)
-//     notesContainer.appendChild(img)
-// })
-// createBtn.addEventListener("click", () => {
-//     let noteWrapper = document.createElement("div"); // Wrapper for note + delete icon
-//     noteWrapper.className = "note-wrapper";
-
-//     let inputBox = document.createElement("p");
-//     inputBox.className = "input-box";
-//     inputBox.setAttribute("contenteditable", "true");
-
-//     let delBtn = document.createElement("img");
-//     delBtn.src = "delete.png";
-//     delBtn.className = "delete-icon";
-
-//     // Append elements
-//     noteWrapper.appendChild(inputBox);
-//     noteWrapper.appendChild(delBtn);
-//     notesContainer.appendChild(noteWrapper);
-
-//     updateStorage();
-// });
 createBtn.addEventListener("click", () => {
     let note = document.createElement("div");
     note.className = "note";
